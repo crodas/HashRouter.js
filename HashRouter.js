8e3b73b636952509b70b8b02e4047c15935aacf6
Cesar Rodas <crodas@php.net>
2015-11-29 18:06:26 -0300

    First commit

---
+++ HashRouter.js
@@ -0,0 +1,168 @@
+var HashRouter = (function(window) {
+    var ns = {};
+    var regex  = [];
+    var routes = [];
+    var routesByName = {};
+    var globalFilter = {};
+
+    function Route(url, callback) {
+        var vars  = {};
+        var varPos = [];
+        var text  = [];
+        if (url instanceof RegExp) {
+            this.url = url;
+            this.callback = callback;
+            return;
+        }
+
+        var parts = url.split(/\/+/g).map(function(part) {
+            if (part.match(/^:[a-z_][a-z0-9_]*$/i)) {
+                vars[part.substr(1)] = {};
+                varPos.push(part.substr(1));
+                text.push(null);
+                return ['/', vars[part.substr(1)]]
+            }
+
+            text.push(part);
+            return ['/', part];
+
+        });
+        parts[0][0] = "#"; // swap the first / for #.
+
+        this.minLength = text.length;
+        this.maxLength = text.length;
+        this.filter = {};
+        this.url    = url;
+        this.parts  = Array.prototype.concat.apply([], parts);
+        this.vars   = vars;
+        this.varPos = varPos;
+        this.text   = text;
+        this.callback = callback;
+    }
+    Route.prototype.name = function(name) {
+        routesByName[name] = this;
+        return this;
+    }
+    Route.prototype.generate = function(args) {
+        var id  = 0;
+        var varPos = this.varPos;
+        return this.parts.map(function(part) {
+            if (typeof part === "object") {
+                var name  = varPos[id];
+                var value = args[id++] || part.default;
+                if (!value) {
+                    throw new Error("cannot find variable " + part + " in the arguments");
+                }
+                if (globalFilter[name] && !globalFilter[name](value)) {
+                    throw new Error(value + " is not a valid " + name);
+                }
+                return value;
+            }
+
+            return part;
+        }).join('');
+        return 
+    }
+    Route.prototype.setDefault = function(name, value) {
+        if (!this.vars.hasOwnProperty(name)) {
+            throw new Error("cannot find variable " + name);
+        }
+        this.vars[name]['default'] = value;
+        return this;
+    }
+
+    Route.prototype.isCandidate = function(parts, vars) {
+        var i, e, x = 0, zvar;
+
+        // Mininum length?
+        if (this.minLength > parts.length || this.maxLength < parts.length) {
+            return false;
+        }
+
+        // Fail sooner, check all the static content (no placeholders/variables)
+        for (i = 0, e = 0; i < parts.length && e < this.text.length; ++e, ++i ) {
+            if (this.text[e] !== null && parts[i] !== this.text[e]) {
+                return false;
+            }
+        }
+
+
+        // check all the variables and their filters/validators
+        for (i = 0, e = 0; i < parts.length && e < this.text.length; ++e, ++i ) {
+            if (this.text[e] === null) {
+                zvar = this.varPos[x++];
+                if (this.filter[zvar] && !this.filter[zvar](parts[e])) {
+                    return false;
+                } else if (globalFilter[zvar] && !globalFilter[zvar](parts[e])) {
+                    return false;
+                }
+                vars.push(parts[e]);
+            }
+        }
+
+        return true;
+    };
+
+    function checkFunction(obj) {
+        if (!(obj instanceof Function)) {
+            throw new Error("callback should be a valid function");
+        }
+    }
+
+    function doRoute() {
+        var parts = document.location.hash.substr(1).split(/\/+/g);
+        var vars;
+        for (var i in routes) {
+            vars = [];
+            if (!routes.hasOwnProperty(i) || !routes[i].isCandidate(parts, vars)) {
+                continue;
+            }
+
+            return routes[i].callback.apply(null, vars);
+        }
+
+        for (var i in regex) {
+            if (regex.hasOwnProperty(i) && document.location.href.match(regex[i].url)) {
+                return regex[i].callback.apply(null, vars);
+            }
+        }
+    }
+
+    if ("addEventListener" in window) {
+        window.addEventListener("hashchange", doRoute, false);
+    } else {
+        window.onhashchange = doRoute;
+    }
+
+    ns.ready = doRoute;
+
+    ns.url = function(name) {
+        if (!(routesByName[name] instanceof Route)) {
+            throw new Error("Cannot find " + name + " url");
+        }
+
+        return routesByName[name].generate(Array.prototype.slice.call(arguments, 1));
+    };
+
+    ns.addFilter = function(name, callback) {
+        checkFunction(callback);
+        globalFilter[name] = callback;
+        return this;
+    };
+
+    ns.route = function(url, callback) {
+        checkFunction(callback);
+        var r = new Route(url, callback);
+        if (url instanceof RegExp) {
+            regex.push(r);
+        } else { 
+            routes.push(r);
+        }
+
+        return r;
+    };
+
+    return ns;
+})(this);
+
+var hRouter = HashRouter;
---
+++ tests.js
@@ -0,0 +1,146 @@
+hRouter.addFilter('page', function(page) {
+    return parseInt(page) > 0;
+});
+
+hRouter.route('foo/bar/:page', function(page) {
+    window.lastPage1 = parseInt(page);
+}).name("page1").setDefault("page", 1);
+
+hRouter.route('foo/bar/:page/:page', function(page1, page2) {
+    window.lastPage2 = [parseInt(page1), parseInt(page2)];
+}).name("page2").setDefault("page", 1);
+
+hRouter.route('foo/:page/bar', function(page) {
+    window.lastPage3 = parseInt(page);
+}).name("page3").setDefault("page", 1);
+
+hRouter.route('foo/bar/:word', function(word) {
+    window.lastWord = word;
+}).name("foobar_word");
+
+hRouter.route(/.+/, function() {
+    window.catchAll = document.location.href;
+});
+
+
+function AsyncLoop(vars, done) {
+    function run() {
+        var fnc = vars.shift();
+        if (!fnc) return done();
+
+        fnc(run);
+    }
+
+    run();
+}
+
+QUnit.test("word", function(assert) {
+    var done = assert.async();
+    function redirect(i) {
+        return function(next) {
+            document.location.href = "#foo/bar/" + i;
+            setTimeout(function() {
+                assert.equal(i, window.lastWord);
+                next();
+            });
+        };
+    }
+    var tests = [];
+    for (var i = 1; i <= 5; i++) {
+        tests.push(redirect("w" + i));
+    }
+    AsyncLoop(tests, done);
+});
+
+QUnit.test("page3", function(assert) {
+    var done = assert.async();
+    function redirect(i) {
+        return function(next) {
+            document.location.href = "#foo/" + i + "/bar";
+            setTimeout(function() {
+                assert.equal(i, window.lastPage3);
+                next();
+            });
+        };
+    }
+    var tests = [];
+    for (var i = 1; i <= 5; i++) {
+        tests.push(redirect(i));
+    }
+    AsyncLoop(tests, done);
+});
+
+
+QUnit.test("page1", function(assert) {
+    var done = assert.async();
+    function redirect(i) {
+        return function(next) {
+            document.location.href = "#foo/bar/" + i;
+            setTimeout(function() {
+                assert.equal(i,  window.lastPage1);
+                next();
+            });
+        };
+    }
+    var tests = [];
+    for (var i = 1; i <= 5; i++) {
+        tests.push(redirect(i));
+    }
+    AsyncLoop(tests, done);
+});
+
+QUnit.test("page2", function(assert) {
+    var done = assert.async();
+    function redirect(i) {
+        return function(next) {
+            document.location.href = "#foo/bar/" + i + "/" + (5+i);
+            setTimeout(function() {
+                assert.equal(i, window.lastPage2[0]);
+                assert.equal(i+5, window.lastPage2[1]);
+                next();
+            });
+        };
+    }
+    var tests = [];
+    for (var i = 1; i <= 5; i++) {
+        tests.push(redirect(i));
+    }
+    AsyncLoop(tests, done);
+});
+
+QUnit.test("catchAll", function(assert) {
+    var done = assert.async();
+    function redirect(i) {
+        return function(next) {
+            document.location.href = "#foo/bar/" + i + "/" + (5+i) + "/" + (5+i);
+            setTimeout(function() {
+                assert.equal(document.location.href, window.catchAll);
+                next();
+            });
+        };
+    }
+    var tests = [];
+    for (var i = 1; i <= 5; i++) {
+        tests.push(redirect(i));
+    }
+    AsyncLoop(tests, done);
+});
+
+QUnit.test("invalid_args_generate_url", function(assert) {
+    for (var i = 1; i < 50; i++) {
+        try {
+            hRouter.url("page1", "w" + i);
+            assert.ok(false);
+        } catch (e) {
+            assert.ok(true);
+        }
+    }
+});
+
+
+QUnit.test("generate_url", function(assert) {
+    for (var i = 1; i < 50; i++) {
+        assert.equal("#foo/bar/" + i, hRouter.url("page1", i));
+        assert.equal("#foo/bar/" + i + "/" + (i+6), hRouter.url("page2", i, i+6));
+    }
+});
